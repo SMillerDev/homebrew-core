2aaa5d326aeb5d6252658d0b48a61e3d667a5667
Sean Molenaar <smillerdev@me.com>
2021-04-01 11:05:57 +0200

    boost-python: move to github checkout

--- Formula/boost-python.rb
+++ Formula/boost-python.rb
@@ -1,9 +1,9 @@
 class BoostPython < Formula
   desc "C++ library for C++/Python2 interoperability"
   homepage "https://www.boost.org/"
-  url "https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2"
-  mirror "https://dl.bintray.com/homebrew/mirror/boost_1_74_0.tar.bz2"
-  sha256 "83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1"
+  url "https://github.com/boostorg/boost.git",
+      tag:      "boost-1.75.0",
+      revision: "b7b1371294b4bdfc8d85e49236ebced114bc1d8f"
   license "BSL-1.0"
   head "https://github.com/boostorg/boost.git"
 
