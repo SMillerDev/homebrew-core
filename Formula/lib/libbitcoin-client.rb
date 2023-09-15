015b43d07ad3c7e7f40e9a712f4a7d248ea092cf
Rui Chen <rui@chenrui.dev>
2023-09-15 00:05:31 -0400

    libbitcoin-client: revision bump (libsodium 1.0.19)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libbitcoin-client.rb
+++ Formula/lib/libbitcoin-client.rb
@@ -4,7 +4,7 @@ class LibbitcoinClient < Formula
   url "https://github.com/libbitcoin/libbitcoin-client/archive/v3.8.0.tar.gz"
   sha256 "cfd9685becf620eec502ad53774025105dda7947811454e0c9fea30b27833840"
   license "AGPL-3.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "fc3651632bac9150341f1bd662863858e80c7ca54b0ddaeaaa1888d0e1f4dbd1"
