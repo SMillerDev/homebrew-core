bf1c95ffb84456c6eb8f842abb8145ffd51dc93b
Rui Chen <rui@chenrui.dev>
2023-09-15 00:05:31 -0400

    libbitcoin-protocol: revision bump (libsodium 1.0.19)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/lib/libbitcoin-protocol.rb
+++ Formula/lib/libbitcoin-protocol.rb
@@ -4,7 +4,7 @@ class LibbitcoinProtocol < Formula
   url "https://github.com/libbitcoin/libbitcoin-protocol/archive/v3.8.0.tar.gz"
   sha256 "654aee258d7e110cce3c445906684f130c7dc6b8be2273c8dab4b46a49d8f741"
   license "AGPL-3.0"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_sonoma:   "6f65941ceb0117dddcdffeba143f12576349b58cee051b2dd9ee9743b3500fdb"
